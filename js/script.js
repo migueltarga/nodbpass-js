ae17465934ca0d87ca45fa9e308eca9657d5f652
Miguel Targa <miguel@games.com>
2016-12-04 11:58:19 -0500

    pass recovery

--- js/script.js
+++ js/script.js
@@ -22,11 +22,25 @@ $(function() {
           $('#password_error').show();
           return;
         }
-        var qntChar = 12;
+        var qntChar = 14;
         var hash = md5($('#username').val()+$('#service').val()+$('#password').val()).substring(0,qntChar);
-        hash = (!/^[a-f]$/.test(hash)) ? hash.replace(/^\d/, $('#username').val().slice(-1).toUpperCase()) : hash.charAt(0).toUpperCase() + hash.slice(1);
+        console.log(hash);
+        hash = $('#username').val().slice(-1).toUpperCase() + hash.slice(1);
         $('#hash').html(hash);
         $('#password_hash').show();
+        $(this).find("input[type=text],input[type=password]").val("");
+    });
+
+  $('#recover-form').on('submit', function(e){
+        e.preventDefault();
+        $('#password_error_2').hide();
+        $('#password_hash_2').hide();
+        var qntChar = 14;
+        var hash = md5($('#username_2').val()+$('#service_2').val()+$('#password_2').val()).substring(0,qntChar);
+        console.log(hash);
+        hash = $('#username_2').val().slice(-1).toUpperCase() + hash.slice(1);
+        $('#hash_2').html(hash);
+        $('#password_hash_2').show();
         $(this).find("input[type=text],input[type=password]").val("");
     });
 });
